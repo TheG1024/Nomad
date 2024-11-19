5f654796ef06d7e6256adf6e2e01ea2fe0c66404
TheG1024 <freedomforextraders@outlook.com>
2024-11-19 13:06:41 +0200

    removed the duplicate ObjectMapper bean

--- src/main/java/com/gpstracker/model/GpsData.java
+++ src/main/java/com/gpstracker/model/GpsData.java
@@ -1,6 +1,10 @@
 package com.gpstracker.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,7 +17,10 @@ public class GpsData implements Serializable {
     private double speed;
     private double heading;
     
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime timestamp;
+    
     private String additionalInfo;
 }
